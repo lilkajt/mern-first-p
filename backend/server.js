8085babd1c43c8aff13b44cff0645c9955a4bd5c
unknown <87129505+lilkajt@users.noreply.github.com>
2024-12-20 13:33:47 +0100

    added /api/products

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 const app = express();
-
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Server is ready');
+});
+
+app.post('/api/products', async (req, res) => {
+    const product = req.body;
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ message: 'Invalid product data' });
+    }
+    const newProduct = new Product(product);
+    try {
+        await newProduct.save();
+        res.status(201).json({success: true, data: newProduct});
+    } catch (error) {
+        console.log(`Error in create product: ${error.message}`);
+        res.status(500).json({success: false, message: error.message});
+    }
 })
 app.listen(5000, ()=>{
     connectDB();
