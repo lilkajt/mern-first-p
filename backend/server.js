b6043624dd8de10ba96503d94ac0cebf7fbecc45
Kajetan <87129505+lilkajt@users.noreply.github.com>
2024-12-22 15:31:15 +0100

    added put

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
 //npm run dev
 const app = express();
 app.use(express.json());
@@ -39,6 +40,21 @@ app.delete('/api/products/:id', async (req, res) => {
         res.status(200).json({success: true, data: deletedProduct});
     } catch (error) {
         console.log(`Error in delete product: ${error.message}`);
+        res.status(500).json({success: false, message: error.message});
+    }
+});
+
+app.put('/api/products/:id', async (req, res) => {
+    const {id} = req.params;
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send('No product with that id');
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
+        res.status(200).json({success: true, data: updatedProduct});
+    } catch (error) {
         res.status(500).json({success: false, message: error.message});
     }
 });
