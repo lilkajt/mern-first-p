dfe0a9b1f4ba3b379dd95a4bba3864fd093ad214
Kajetan <87129505+lilkajt@users.noreply.github.com>
2024-12-22 12:43:12 +0100

    added getProducts and deleteProduct

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
-
+//npm run dev
 const app = express();
 app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Server is ready');
+});
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({success: true, data: products});
+    } catch (error) {
+        console.log(`Error in get products: ${error.message}`);
+        res.status(500).json({success: false, message: error.message});
+    }
 });
 
 app.post('/api/products', async (req, res) => {
@@ -21,8 +31,19 @@ app.post('/api/products', async (req, res) => {
         console.log(`Error in create product: ${error.message}`);
         res.status(500).json({success: false, message: error.message});
     }
-})
-app.listen(5000, ()=>{
+});
+
+app.delete('/api/products/:id', async (req, res) => {
+    try {
+        const deletedProduct = await Product.findByIdAndRemove(req.params.id);
+        res.status(200).json({success: true, data: deletedProduct});
+    } catch (error) {
+        console.log(`Error in delete product: ${error.message}`);
+        res.status(500).json({success: false, message: error.message});
+    }
+});
+
+app.listen(7500, ()=>{
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:7500");
 });
