7e0311febe588a337cad1b30ea66a36c58af8a99
Kajetan <87129505+lilkajt@users.noreply.github.com>
2024-12-26 12:18:58 +0100

    HomePage no products style

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,37 @@
+import { Container, Text, VStack } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
+
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <Container maxW={"container.xl"} py={12}>
+      <VStack spacing={8}>
+        <Text
+        fontSize={30}
+        fontWeight={"bold"}
+        textAlign={"center"}
+        bgGradient={"to-r"}
+        gradientFrom={"cyan.400"}
+        gradientTo={"blue.500"}
+        bgClip={"text"}
+        >
+          Current Products 🚀
+        </Text>
+
+        <Text
+        fontSize={"xl"}
+        textAlign={"center"}
+        fontWeight={"bold"}
+        color={"gray.500"}
+        >
+          No product found {"😢"}
+          <Link to={"/create"}>
+            <Text as={"span"} color={"blue.500"} _hover={{textDecoration: "underline"}}>
+              Create a product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
   )
 }
 
