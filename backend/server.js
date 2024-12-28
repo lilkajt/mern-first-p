1cbe3b1874f02ba0a4f65a44f51ba4b73f9240d3
Kajetan <87129505+lilkajt@users.noreply.github.com>
2024-12-28 15:09:30 +0100

    ready production build

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
-//npm run dev
+import path from 'path';
 const app = express();
+const __dirname = path.resolve();
 app.use(express.json());
 app.use("/api/products", productRoutes);
+if ( process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+}
 app.listen(7500, ()=>{
     connectDB();
     console.log("Server started at http://localhost:7500");
